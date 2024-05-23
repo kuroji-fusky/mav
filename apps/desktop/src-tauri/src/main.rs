01b4d167a681ac6decc01bf1df6fb00d013aa680
Kerby Keith Aquino <hello@kurojifusky.com>
2024-05-23 21:49:28 +0800

    minor: tauri and rust stuff

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,9 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
+    let toggle_window = CustomMenuItem::new("toggleWindow".to_string(), "Hide/Show MyArtverse");
+    let login_user = CustomMenuItem::new("login".to_string(), "Login/Register to MyArtverse");
+    let logout_user = CustomMenuItem::new("logout".to_string(), "Log out...");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(toggle_window)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(login_user)
+        .add_item(logout_user)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit)
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(SystemTray::new().with_menu(tray_menu));
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
