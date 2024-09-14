b97c6c5cdf1a37f39d74fdc82554fd979492f6dc
Kerby Keith Aquino <hello@kurojifusky.com>
2024-09-14 11:01:32 +0800

    desktop: add sys tray event

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -4,9 +4,15 @@
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
-    let toggle_window = CustomMenuItem::new("toggleWindow".to_string(), "Hide/Show MyArtverse");
-    let login_user = CustomMenuItem::new("login".to_string(), "Login/Register to MyArtverse");
-    let logout_user = CustomMenuItem::new("logout".to_string(), "Log out...");
+    let brand_name: &str = "MyArtverse".to_owned();
+
+    let main_window_process = app.get_window("main");
+    let window = main_window_process.unwrap();
+
+    // System Tray stuff
+    let toggle_window = CustomMenuItem::new("toggleWindow".to_string(), format!("Hide/Show {brand_name}"));
+    let login_user = CustomMenuItem::new("login".to_string(), format!("Login/Register to {brand_name}"));
+    let logout_user = CustomMenuItem::new("logout".to_string(), "Log out");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     
     let tray_menu = SystemTrayMenu::new()
@@ -18,7 +24,30 @@ fn main() {
         .add_item(quit);
 
     tauri::Builder::default()
-        .system_tray(SystemTray::new().with_menu(tray_menu));
         .run(tauri::generate_context!());
+        // System Tray stuff
+        .system_tray(SystemTray::new().with_menu(tray_menu));
+        .on_system_tray_event(|app, event| match event {
+          SystemTrayEvent::DoubleClick {
+            position: _,
+            size: _,
+            ..
+          } => {
+            println!("system tray received a double click");
+            window.open().unwrap();
+          }
+          SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+              "quit" => {
+                std::process::exit(0);
+              }
+              "hide" => {
+                window.hide().unwrap();
+              }
+              _ => {}
+            }
+          }
+          _ => {}
+        })
         .expect("error while running tauri application");
 }
